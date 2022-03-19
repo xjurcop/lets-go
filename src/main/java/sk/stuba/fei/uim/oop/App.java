4ca841df489d793ea2e3f92581d8884a4fc2a20c
xjurcop <xjurcop@stuba.sk>
2022-03-19 14:44:16 +0100

    Revert "hello world"
    This reverts commit d799c9a5

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,17 +2,6 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        int[] pole = new int[10];
-
-        for (int i = 0; i <pole.length; i++){
-            pole[i] = i;
-        }
-        for (int i = 0; i <pole.length; i++){
-          vypis(i);
-        }
-    }
-
-    public static void vypis(int a){
-        System.out.println(a);
+        System.out.println("Hello World");
     }
 }
